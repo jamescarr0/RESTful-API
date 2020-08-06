291db0e469d9338ffc04eebbb56e719bb24ede60
James Carr <jamescarr0@hotmail.com>
2020-08-06 20:53:02 +0100

    Mongoose connect, schema and model complete

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const app = express()
-
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded( { extended: true }))
 app.set('view engine', 'ejs')
+
+// Connect to mongoDB.
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useUnifiedTopology: true, useNewUrlParser: true })
+
+// Create a new article schema.
+const articleSchema = {
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}
+
+// Create a new article model. ("Collection name", Schema)
+const Article = mongoose.model("Article", articleSchema)
 
 app.get('/', (req, res) => {
     res.send('Server up and running')
