308cca7d604087f18ed5107d066c942378cf15c1
James Carr <jamescarr0@hotmail.com>
2020-08-06 21:18:04 +0100

    fetch all articles for article route complete.

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.urlencoded( { extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 // Connect to mongoDB.
+
 mongoose.connect("mongodb://localhost:27017/wikiDB", { useUnifiedTopology: true, useNewUrlParser: true })
 
 // Create a new article schema.
@@ -22,13 +23,17 @@ const articleSchema = {
     }
 }
 
-// Create a new article model. ("Collection name", Schema)
+// Create a new article model. ("Collection name", Schema).
 const Article = mongoose.model("Article", articleSchema)
 
-app.get('/', (req, res) => {
-    res.send('Server up and running')
+app.get('/articles', (req, res) => {
+    Article.find((error, results) => {
+        if(error) { results = error }
+        res.send(results)
+    })
 })
 
+// Start server.
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
