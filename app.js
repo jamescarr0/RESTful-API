f0147534dd97c7d5308a9a1d9bb3c8543c69b295
James Carr <jamescarr0@hotmail.com>
2020-08-07 18:59:07 +0100

    POST route and save to database complete

--- app.js
+++ app.js
@@ -8,19 +8,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 // Connect to mongoDB.
-
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useUnifiedTopology: true, useNewUrlParser: true }, function (error, db) {
+    if (error) { console.log("Connection failed with - " + error) }
+    else { console.log(`Successfully connected to database - '${db.name}'`) }
+})
 
 // Create a new article schema.
 const articleSchema = {
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    }
+    title: { type: String, required: true },
+    content: { type: String, required: true }
 }
 
 // Create a new article model. ("Collection name", Schema).
@@ -28,8 +24,23 @@ const Article = mongoose.model("Article", articleSchema)
 
 app.get('/articles', (req, res) => {
     Article.find((error, results) => {
-        if(error) { results = error }
+        if (error) { console.log(error) }
         res.send(results)
+    })
+})
+
+app.post('/articles', (req, res) => {
+
+    // Create a new article document.
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    })
+
+    // Insert article document into database.
+    newArticle.save((error, doc) => {
+        if (error) { res.send(`Error inserting document: ${newArticle}\n${error} `) }
+        else { res.send(`Document inserted successfully:\n${doc}\n`) }
     })
 })
 
